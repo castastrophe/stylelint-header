22312532ab5f4ac2f056cece8092c572ee86a614
castastrophe <castastrophe@users.noreply.github.com>
2023-10-26 11:42:45 -0400

    feat: updated commenting syntax; more robust testing

--- index.js
+++ index.js
@@ -1,121 +1,134 @@
-/*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
+/*!
+ * Copyright 2023. All rights reserved.
+ *
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
+const { existsSync, readFileSync } = require("fs");
+const { join } = require("path");
 
-const { existsSync, readFileSync } = require('fs');
-const { createPlugin, utils } = require('stylelint');
-const { compareTwoStrings } = require('string-similarity');
-const { template } = require('lodash');
+const { createPlugin, utils } = require("stylelint");
 
-const ruleName = 'header/header';
+const { compareTwoStrings } = require("string-similarity");
+const { template } = require("lodash");
+
+const ruleName = "header/header";
 
 const messages = utils.ruleMessages(ruleName, {
-    rejected: 'Header not found',
+	rejected: "Header not found",
 });
 
-const rule = createPlugin(
-    ruleName,
-    (pathOrString, options = {}, context = {}) =>
-        (root, result) => {
-            if (typeof pathOrString === 'boolean' && !pathOrString) return;
+const plugin = createPlugin(
+	ruleName,
+	(pathOrString, options = {}, context = {}) =>
+		(root, result) => {
+			const validOptions = utils.validateOptions(
+				result,
+				ruleName,
+				{
+					actual: pathOrString,
+					possible: [
+						null,
+						(x) => typeof x === "string",
+						(x) => typeof x === "string" && existsSync(x),
+						(x) => typeof x === "string" && existsSync(join(process.cwd(), x)),
+					],
+				},
+				{
+					optional: true,
+					actual: options,
+					possible: {
+						nonMatchingTolerance: [
+							(val) => typeof val === "number" && val >= 0 && val <= 1,
+						],
+						templateVariables: [Object, null],
+					},
+				}
+			);
 
-            let headerTemplate = existsSync(pathOrString)
-                ? readFileSync(pathOrString, 'utf8')
-                : pathOrString;
+			if (!validOptions) return;
 
-            if (!headerTemplate) return;
+			let headerTemplate = existsSync(pathOrString)
+				? readFileSync(pathOrString, "utf8")
+				: existsSync(join(process.cwd(), pathOrString))
+				? readFileSync(join(process.cwd(), pathOrString), "utf8")
+				: pathOrString;
 
-            // Trim any comment tags from the string and remove whitespace
-            headerTemplate = headerTemplate.replace(/(\/\*|\*\/)/g, '').trim();
+			if (!headerTemplate) return;
 
-            const validOptions = utils.validateOptions(
-                result,
-                ruleName,
-                {
-                    actual: pathOrString,
-                    possible: [
-                        false,
-                        (x) => typeof x === 'string' && existsSync(x),
-                    ],
-                },
-                {
-                    optional: true,
-                    actual: options,
-                    possible: {
-                        nonMatchingTolerance: [
-                            (x) => typeof x === 'number' && (x >= 0 || x <= 1),
-                        ],
-                        templateInputs: [(x) => typeof x === 'object'],
-                    },
-                }
-            );
+			// Trim any comment tags from the string and remove whitespace
+			headerTemplate = headerTemplate
+				.replace(/(\/\*|\*\/|(\s*\*))/g, "")
+				.trim();
 
-            if (!validOptions) return;
+			const getHeader = template(headerTemplate);
+			const header = getHeader({
+				YEAR: new Date().getFullYear(),
+				FILE_NAME: context.file?.basename,
+				FILE_PATH: context.file?.dirname,
+				...(options.templateVariables ?? {}),
+			});
 
-            const getHeader = template(headerTemplate);
-            const header = getHeader({
-                YEAR: new Date().getFullYear(),
-                ...options.templateInputs,
-            });
+			const nonMatchingTolerance = options?.nonMatchingTolerance || 0.98;
 
-            const nonMatchingTolerance = options?.nonMatchingTolerance || 0.98;
+			// Walk comments on root to find if header exists
+			let found = false;
+			root.walkComments((comment, _idx) => {
+				// Remove any asterisks and whitespace from the texts before comparing
+				const clean = (text) =>
+					text
+						.replace(/(\*|\n|\s)/g, "")
+						.replace(/^!/g, "")
+						.trim();
 
-            // Walk comments on root to find if header exists
-            let found = false;
-            root.walkComments((comment, _idx) => {
-                // Remove any asterisks and whitespace from the texts before comparing
-                const clean = (text) => text.replace(/(\*|\n|\s)/g, '').trim();
-                // If the two strings are at least 98% alike, it's a match
-                if (
-                    compareTwoStrings(clean(comment.text), clean(header)) >=
-                    nonMatchingTolerance
-                ) {
-                    found = true;
-                }
+				// If the two strings are at least 98% alike, it's a match
+				if (
+					compareTwoStrings(clean(comment.text), clean(header)) >=
+					nonMatchingTolerance
+				) {
+					found = true;
+				}
 
-                // This escapes the loop if found, continues if not found
-                return !found;
-            });
+				// This escapes the loop if found, continues if not found
+				return !found;
+			});
 
-            if (found) return;
+			if (found) return;
 
-            if (context.fix) {
-                const lineLength = header.split('\n').length;
-                let raws = {};
-                if (lineLength > 1) {
-                    raws = {
-                        left: context.newline,
-                        right: context.newline,
-                    };
-                }
-                // Add the provided header to the top of the file
-                root.prepend({
-                    text: header,
-                    raws,
-                });
-                // Put a few newlines between the comment and the first property
-                root.nodes[1].raws.before = context.newline + context.newline;
-            } else {
-                // Just report the issue
-                utils.report({
-                    ruleName: ruleName,
-                    result: result,
-                    message: messages.rejected,
-                    node: root,
-                });
-            }
-        }
+			if (context.fix) {
+				// Add the provided header to the top of the file
+				root.prepend({
+					text: header
+						.split("\n")
+						.map((line) => ` * ${line}`)
+						.join("\n"),
+					raws: {
+						left: "!\n",
+						right: "\n ",
+					},
+				});
+				// Put a few newlines between the comment and the first property
+				root.nodes[1].raws.before = context.newline + context.newline;
+			} else {
+				// Just report the issue
+				utils.report({
+					ruleName: ruleName,
+					result: result,
+					message: messages.rejected,
+					node: root,
+				});
+			}
+		}
 );
 
-rule.ruleName = ruleName;
-rule.messages = messages;
+plugin.ruleName = ruleName;
+plugin.messages = messages;
 
-module.exports = rule;
+module.exports = plugin;
--- test/stylelint-header.test.js
+++ test/stylelint-header.test.js
@@ -10,68 +10,125 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const { readFileSync } = require('fs');
-const { getTestRule } = require('jest-preset-stylelint');
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const { getTestRule } = require("jest-preset-stylelint");
 
-const plugin = require('..');
+const plugin = require("..");
 const { messages, ruleName } = plugin;
 
-const testRule = getTestRule({ plugins: ['.'] });
+const testRule = getTestRule({ plugins: ["."] });
 
 testRule({
-    ruleName,
-    config: [
-        './input.txt',
-        {
-            nonMatchingTolerance: 1,
-            templateVariables: {
-                company: 'Adobe',
-            },
-        },
-    ],
-    fix: true,
+	ruleName,
+	config: [
+		"./test/input.txt",
+		{
+			nonMatchingTolerance: 1,
+			templateVariables: {
+				company: "Adobe",
+			},
+		},
+	],
+	fix: true,
 
-    accept: [
-        {
-            code: readFileSync('test/pass.css', { encoding: 'utf-8' }),
-            description: 'Simple CSS with header included',
-        },
-    ],
+	accept: [
+		{
+			code: readFileSync("test/pass.css", { encoding: "utf-8" }),
+			description: "Simple CSS with header included",
+		},
+	],
 
-    reject: [
-        {
-            code: readFileSync('test/fail.css', { encoding: 'utf-8' }),
-            fixed: readFileSync('test/fixed.css', { encoding: 'utf-8' }),
-            description: 'Auto-fix file missing header',
-            message: messages.rejected,
-        },
-    ],
+	reject: [
+		{
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+			fixed: readFileSync("test/fixed.css", { encoding: "utf-8" }),
+			description: "Auto-fix file missing header",
+			message: messages.rejected,
+		},
+	],
 });
 
 testRule({
-    ruleName,
-    config: [
-        'Copyright <%= YEAR %> <%= company %>.',
-        {
-            nonMatchingTolerance: 1,
-            templateVariables: {
-                company: 'Adobe',
-            },
-        },
-    ],
+	ruleName,
+	config: [
+		"Copyright <%= YEAR %> <%= company %>.",
+		{
+			nonMatchingTolerance: 1,
+			templateVariables: {
+				company: "Adobe",
+			},
+		},
+	],
 
-    accept: [
-        {
-            code: readFileSync('test/pass.css', { encoding: 'utf-8' }),
-            description: 'Simple CSS with header included',
-        },
-    ],
+	accept: [
+		{
+			code: readFileSync("test/pass.css", { encoding: "utf-8" }),
+			description: "Simple CSS with header included",
+		},
+	],
 
-    reject: [
-        {
-            code: readFileSync('test/fail.css', { encoding: 'utf-8' }),
-            description: 'Error if missing header',
-            message: messages.rejected,
-        },
-    ],
+	reject: [
+		{
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+			description: "Error if missing header",
+			message: messages.rejected,
+		},
+	],
+});
+
+testRule({
+	ruleName,
+	config: [join(__dirname, "../COPYRIGHT")],
+
+	accept: [
+		{
+			code: readFileSync("test/multi-line.css", { encoding: "utf-8" }),
+			description: "Simple CSS with multi-line header",
+		},
+	],
+
+	reject: [
+		{
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+			description: "Error if missing header",
+			message: messages.rejected,
+		},
+	],
+});
+
+testRule({
+	ruleName,
+	config: null,
+	accept: [
+		{
+			description: "Rule disabled",
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+		},
+	],
+});
+
+testRule({
+	ruleName,
+	config: true,
+	reject: [
+		{
+			description: "Invalid inputs",
+			message: 'Invalid option value "true" for rule "header/header"',
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+		},
+	],
+});
+
+testRule({
+	ruleName,
+	config: ["Copyright <%= YEAR %> Adobe.", { nonMatchingTolerance: 10 }],
+	reject: [
+		{
+			description: "Invalid input for nonMatchingTolerance",
+			message:
+				'Invalid value "10" for option "nonMatchingTolerance" of rule "header/header"',
+			code: readFileSync("test/fail.css", { encoding: "utf-8" }),
+		},
+	],
 });
